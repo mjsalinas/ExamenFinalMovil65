2b52b8700fa605e6c548733e81dfca8e798b9627
Examen2 Student <student@example.com>
2026-09-21 19:54:00 -0600

    Configure Supabase env and Redux store

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
+import 'react-native-url-polyfill/auto';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import ExpensesScreen from './src/screens/Expenses';
+import { store } from './src/store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <Provider store={store}>
       <StatusBar style="auto" />
-    </View>
+      <ExpensesScreen />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
