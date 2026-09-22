75bee01b0be068b66290cb420881cba9792e6db0
Stefany <stefany_madrid@unitec.edu>
2026-09-21 20:37:07 -0600

    -implementacion de conexion con la base de datos supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,13 +1,14 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? '';
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? '';
+   const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? '';
+    const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? '';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    persistSession: false,
-    autoRefreshToken: false,
-    detectSessionInUrl: false,
+    export const supabase =createClient(supabaseUrl, supabaseAnonKey, {
+      auth: { 
+        persistSession:  false,
+        autoRefreshToken: false,
+        detectSessionInUrl: false, 
   },
-});
+}); 
+
