afe8a62f0f501a5c79991fb6c0afbdbce7d37eb9
ot0h <rimv715@gmail.com>
2026-09-21 19:49:04 -0600

    feat: Configurando la store y los hooks de redux

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import { Provider } from 'react-redux'
+import { store } from './src/store'
+
+export function MainScreen() {
+  return <View style={styles.container}></View>
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <MainScreen />
+    </Provider>
   )
 }
 
