6c3941c2a4d610953e33a67dd3dba4fdfe6c0f69
CasasolaHub17 <luiscasasola@unitec.edu>
2026-09-22 23:39:04 -0600

    coneccion a SB

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? '';
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? '';
+const supabaseUrl = 'https://kjtdyxuclhphrxuwmve.supabase.co';
+const supabaseAnonKey = 'sb_publishable_q-1AF3BiOFIpirSmSkVz2A_e-_WZpjh'; 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
